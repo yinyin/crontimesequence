a525642797cff307b49428d137a3869f5003b39c
Yinyin L <yinyinl@gmail.com>
2019-03-01 03:12:19 +0800

    target to 3.6 and 3.7

--- setup.py
+++ setup.py
@@ -27,8 +27,8 @@ setup(name='crontimesequence',
 			'Programming Language :: Python :: 3.0',
 			'Programming Language :: Python :: 3.1',
 			'Programming Language :: Python :: 3.2',
-			'Programming Language :: Python :: 3.3',
-			'Programming Language :: Python :: 3.4', ],
+			'Programming Language :: Python :: 3.6',
+			'Programming Language :: Python :: 3.7', ],
 		license='MIT License',
 	)
 
